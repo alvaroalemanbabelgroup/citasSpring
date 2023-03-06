382fdbbbcd7afbbee61d1ec40b232292f12a7f9d
Alvaro Aleman Rando <alvaro.aleman@babelgroup.com>
2023-03-06 11:51:55 +0100

    Initial commit

---
+++ src/test/java/com/example/citasSpring/CitasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citasSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
